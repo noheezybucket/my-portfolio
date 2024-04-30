3b5789d28b1f16138fa4144673265139a94c3209
Mouhamad Gueye <seydinag023@gmail.com>
2024-04-30 12:34:52 +0000

    projects box updated

--- components/ProjectsBox.jsx
+++ components/ProjectsBox.jsx
@@ -7,7 +7,7 @@ const projects = [
   {
     id: 1,
     title: "RRTACU",
-    description: "Ma Description",
+    description: "",
     image: "rrtacu.png",
     link: "https://rrtacu.com",
     tools: [{ id: 1, name: "WordPress", icon: "wordpress" }],
@@ -15,7 +15,7 @@ const projects = [
   {
     id: 2,
     title: "Ndoura Traiteur",
-    description: "Ma Description",
+    description: "",
     image: "ndoura.png",
     link: "https://ndouratraiteur.com",
     tools: [
@@ -35,39 +35,44 @@ const ProjectsBox = () => {
         // buttonLink={"/projects"}
         // buttonName={"See Everything"}
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="gap-5 border-op p-2">
         {projects.map((project) => {
           return (
-            <Link
-              href={project.link}
-              className="flex flex-col  gap-5 border border-op rounded-xl p-2"
-              key={project.id}
-            >
-              <img
-                src={`/assets/${project.image}`}
-                className="object-cover rounded-xl border border-white border-opacity-20"
-              />
-
-              <div className="flex flex-col gap-2">
-                <h3 className="text-2xl font-semibold">{project.title}</h3>
-                <p className="text-sm">{project.description}</p>
-                <div className="flex gap-3 flex-wrap">
-                  {project.tools.map((tool) => {
-                    return (
-                      <span key={tool.id} className="tools">
-                        <Image
-                          src={`/assets/tools/${tool.icon}.svg`}
-                          width={25}
-                          height={25}
-                          alt="me"
-                        />
-                        <span className="tool-name">{tool.name}</span>
-                      </span>
-                    );
-                  })}
+            <>
+              <Link
+                href={project.link}
+                className="flex  gap-5 rounded-xl hover:translate-x-2 duration-200"
+                key={project.id}
+              >
+                <div className="w-4/12">
+                  <img
+                    src={`/assets/${project.image}`}
+                    className="object-cover rounded-xl border border-white border-opacity-20"
+                  />
                 </div>
-              </div>
-            </Link>
+
+                <div className="flex flex-col gap-2">
+                  <h3 className="text-2xl font-semibold">{project.title}</h3>
+                  <p className="text-sm">{project.description}</p>
+                  <div className="flex gap-3 flex-wrap">
+                    {project.tools.map((tool) => {
+                      return (
+                        <span key={tool.id} className="tools">
+                          <Image
+                            src={`/assets/tools/${tool.icon}.svg`}
+                            width={25}
+                            height={25}
+                            alt="me"
+                          />
+                          <span className="tool-name">{tool.name}</span>
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+              </Link>
+              {projects.length != project.id && <hr className="my-5" />}
+            </>
           );
         })}
       </div>
